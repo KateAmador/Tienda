ec1ac613607afefadd533ed4c4f415bc80d21c8d
ronal2003 <ronuldox.2003@gmail.com>
2022-07-18 17:19:16 -0500

    [MVCVsita]: Se sube por segunda vez el mvc de Factura

--- src/util/Conexion.java
+++ src/util/Conexion.java
@@ -9,7 +9,7 @@ public class Conexion {
 		private static final String CONTROLADOR = "org.postgresql.Driver";
 		private static final String URL = "jdbc:postgresql://localhost:5432/Tienda";
 		private static final String USUARIO = "postgres";
-		private static final String CLAVE = "1234";
+		private static final String CLAVE = "123456";
 		Connection conexion = null;
 
 		static {
