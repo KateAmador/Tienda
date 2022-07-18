b2560ddf724761794c8b7dcd3349454cb7c16e7e
Ronal2003 <Ronuldox.2003@gmail.com>
2022-07-18 10:39:11 -0500

    [mvcFactura]:primera subida del proyecto del mvc de factura

--- src/util/Conexion.java
+++ src/util/Conexion.java
@@ -5,34 +5,33 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Conexion {
+		
+		private static final String CONTROLADOR = "org.postgresql.Driver";
+		private static final String URL = "jdbc:postgresql://localhost:5432/Tienda";
+		private static final String USUARIO = "postgres";
+		private static final String CLAVE = "1234";
+		Connection conexion = null;
 
-	private static final String CONTROLADOR = "org.postgresql.Driver";
-	private static final String URL = "jdbc:postgresql://localhost:5432/tienda";
-	private static final String USUARIO = "postgres";
-	private static final String CLAVE = "1234";
-	Connection conexion = null;
-
-	static {
-		try {
-			Class.forName(CONTROLADOR);
-		} catch (ClassNotFoundException e) {
-			System.out.println("Error a cargar el controlador");
-			e.printStackTrace();
+		static {
+			try {
+				Class.forName(CONTROLADOR);
+			} catch (ClassNotFoundException e) {
+				System.out.println("Error a cargar el controlador" + e);
+				e.printStackTrace();
+			}
 		}
-	}
 
-	public Connection conectar() {
+		public Connection conectar() {
 
-		try {
+			try {
 
-			conexion = DriverManager.getConnection(URL, USUARIO, CLAVE);
-			//System.out.println("Conexion OK");
+				conexion = DriverManager.getConnection(URL, USUARIO, CLAVE);
+				//System.out.println("Conexion OK");
 
-		} catch (SQLException e) {
-			System.out.println("Error en la conexion");
-			e.printStackTrace();
+			} catch (SQLException e) {
+				System.out.println("Error en la conexion");
+				e.printStackTrace();
+			}
+			return conexion;
 		}
-		return conexion;
-	}
-
 }
